2ed7fbef30c2003496b057fae0d520146993f3b7
ricardoadams-cmd <ricardo.adams.jr@gmail.com>
2025-11-26 12:02:41 -0500

    Update server.js

--- templatevault/server.js
+++ templatevault/server.js
@@ -1,10 +1,15 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Optional: keep a root route if you want
 app.get('/', (req, res) => {
-  res.send('Hello World from Render!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
