868a912ba38c6e747b6a8fb1cf7fcde82f4bef4a
ricardoadams-cmd <ricardo.adams.jr@gmail.com>
2025-11-27 09:09:34 -0500

    fd

---
+++ templatevault/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhsMRHyuWF-R9MyRKKjzGQC0p-eznYILE",
+  authDomain: "uplift-local.firebaseapp.com",
+  projectId: "uplift-local",
+  storageBucket: "uplift-local.firebasestorage.app",
+  messagingSenderId: "615612260052",
+  appId: "1:615612260052:web:c7cac371c0698314e36541"
+};
+
+export default firebaseConfig;
