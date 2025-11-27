2af625aee097f521bcd8d27f293a35f7608e2ba8
Ric <153478972+RicDahruler@users.noreply.github.com>
2025-11-27 00:25:10 -0500

    Added setup code

--- templatevault/firebaseConfig.js
+++ templatevault/firebaseConfig.js
@@ -1,3 +1,8 @@
+// Import Firebase SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBhsMRHyuWF-R9MyRKKjzGQC0p-eznYILE",
   authDomain: "uplift-local.firebaseapp.com",
@@ -6,3 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "615612260052",
   appId: "1:615612260052:web:c7cac371c0698314e36541"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
