f358c92a31000268c5ab8462bc2f984dab918ca3
ricardoadams-cmd <ricardo.adams.jr@gmail.com>
2025-11-26 21:58:10 -0500

    updated firbase file

--- templatevault/firebase.js
+++ templatevault/firebase.js
@@ -1,14 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBhsMRHyuWF-R9MyRKKjzGQC0p-eznYILE",
+  authDomain: "uplift-local.firebaseapp.com",
+  projectId: "uplift-local",
+  storageBucket: "uplift-local.firebasestorage.app",
+  messagingSenderId: "615612260052",
+  appId: "1:615612260052:web:c7cac371c0698314e36541"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
