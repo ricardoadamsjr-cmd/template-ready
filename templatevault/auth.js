17d4e08ca5c3b18071ce4d324394e83e280ea603
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-11-30 13:08:02 -0500

    Added Crid

--- templatevault/auth.js
+++ templatevault/auth.js
@@ -1,10 +1,12 @@
 // Initialize Firebase
 // Replace with your Firebase project config (from Firebase Console)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBhsMRHyuWF-R9MyRKKjzGQC0p-eznYILE",
+  authDomain: "uplift-local.firebaseapp.com",
+  projectId: "uplift-local",
+  storageBucket: "uplift-local.firebasestorage.app",
+  messagingSenderId: "615612260052",
+  appId: "1:615612260052:web:c7cac371c0698314e36541"
 };
 
 // Start Firebase
