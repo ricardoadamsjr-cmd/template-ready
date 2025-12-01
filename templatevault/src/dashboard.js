659d8923e2e0fe7de31eed24a25efb7434aca240
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-12-01 13:18:15 -0500

    Download button
    Change id name of button from download-btn to downloadbtn

--- templatevault/src/dashboard.js
+++ templatevault/src/dashboard.js
@@ -1,11 +1,11 @@
 import { auth, db } from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
-
+// this code below checks the user's subscription status and updates the UI accordingly
 async function checkSubscription() {
   const uid = auth.currentUser.uid;
   const userDoc = await getDoc(doc(db, "users", uid));
   const status = userDoc.data()?.subscriptionStatus;
-
+//toggle UI elements based on subscription status
   if (status === "active") {
     document.getElementById("downloadBtn").style.display = "block";
     document.getElementById("subscribeBtn").style.display = "none";
