a9403ef436cbe88b7efbec423177b5b279e7b018
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-12-07 14:04:42 -0500

    Fixed storage bucker link

--- templatevault/src/dashboard.js
+++ templatevault/src/dashboard.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 
 // Initialize Firebase (make sure you have your config object here)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyBhsMRHyuWF-R9MyRKKjzGQC0p-eznYILE",
+    authDomain: "uplift-local.firebaseapp.com",
+    projectId: "uplift-local",
+    storageBucket: "uplift-local.appspot.com",
+    messagingSenderId: "615612260052",
+    appId: "1:615612260052:web:c7cac371c0698314e36541"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
