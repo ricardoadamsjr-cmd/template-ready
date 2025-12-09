14095cb560f9d0a13de078f235ac3055b732c85d
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-12-09 15:49:28 -0500

    fds

--- templatevault/functions/index.js
+++ templatevault/functions/index.js
@@ -1,14 +1,5 @@
-// functions/index.js
-// This file is the entry point for Firebase Functions.
-// It imports your function definitions from auth.js and webhook.js
-// and re‑exports them so Firebase can deploy them.
-
-// functions/index.js
-
 import { createCheckoutSession } from "./auth.js";
 import { stripeWebhook } from "./webhook.js";
-
-// Proper re‑export syntax
 export { createCheckoutSession, stripeWebhook };
 
 
