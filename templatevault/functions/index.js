4101e8b1607f20268c45df09d5bf0bf3c442adfd
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-12-10 17:29:32 -0500

    variables

--- templatevault/functions/index.js
+++ templatevault/functions/index.js
@@ -1,8 +1,8 @@
-import { createCheckoutSession } from "./auth.js";
-import { stripeWebhook } from "./webhook.js";
-export { createCheckoutSession, stripeWebhook };
+const {onRequest} = require("firebase-functions/v2/https");
+const {logger} = require("firebase-functions/logger");
 
-
-
-
-
+// Example usage - create a simple HTTP function
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
