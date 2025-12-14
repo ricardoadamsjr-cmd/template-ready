a410767a2fae89dd5f5813b41b3def7ffe456076
ricardoadamsjr-cmdgit config --user.email ricardo.adams.jr@gmail.com <ricardo.adams.jr@gmail.com>
2025-12-14 14:58:50 -0500

    added image css in dashboard

--- templatevault/cloud-functions/index.js
+++ templatevault/cloud-functions/index.js
@@ -275,7 +275,4 @@ async function handlePaymentFailed(invoice) {
   console.log(`❌ Payment failed for ${firebaseUID}`);
 }
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
